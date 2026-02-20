099d3f9b1bce645913623095fa52a7fd3ad30691
Nati <nsisay49@gmail.com>
2026-02-20 23:15:43 +0300

    clean version

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,5 @@
-"use client";
+import { ComponentExample } from "@/components/component-example";
 
-import WelcomeHeader from "@/components/Navbar";
-import React from "react";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { Play } from "lucide-react";
-import Link from "next/link";
-
-const page = () => {
-  return (
-    <main>
-      <WelcomeHeader />
-      <div className="my-10 sm:flex gap-8">
-        <div className="sm:w-[40%] px-20 sm:py-10 ">
-          <h1>Explore all the Features and use them for free </h1>
-          <p className="text-[40px] font-extrabold">
-            University Students Management Platform
-          </p>
-          <div className="flex items-center mt-20 gap-5">
-            <Button variant="secondary" size="lg">
-              <Link href="/dashboard">Login</Link>
-            </Button>
-            <Link href="/Installation">
-              <Button variant="outline" size="lg">
-                <Play /> Steps to Install
-              </Button>
-            </Link>
-          </div>
-        </div>
-        <div className="bg-white sm:w-[60%] p-5">
-          <Image
-            src="/homeFront.png"
-            alt="home screen image"
-            width={600}
-            height={600}
-          />
-        </div>
-      </div>
-      <p className="text-center text-gray-400 "> copy right &copy; 2025 </p>
-    </main>
-  );
-};
-
-export default page;
+export default function Page() {
+return <ComponentExample />;
+}
