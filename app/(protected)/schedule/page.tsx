5943e65a502ec6443d49b5a9b1c4f41a4311c0de
Nati <nsisay49@gmail.com>
2026-02-23 11:19:08 +0300

    calendar with clear

--- app/(protected)/schedule/page.tsx
+++ app/(protected)/schedule/page.tsx
@@ -1,4 +1,17 @@
 import Calendar from "@/components/Calendar"
+import { Button } from "@/components/ui/button"
+import { BrushCleaning } from "lucide-react"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 const page = () => {
   return (
@@ -8,7 +21,32 @@ const page = () => {
         Stay updated with daily meals,
         serving times, and special menus all in one place.
       </p>
-      <Calendar />
+      <div>
+        <div className="flex justify-end">
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button>
+                <BrushCleaning />
+                Clear
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone.
+                  This will permanently remove all the saved cafeteria schedule.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction>Continue</AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </div>
+        <Calendar />
+      </div>
     </main>
   )
 }
