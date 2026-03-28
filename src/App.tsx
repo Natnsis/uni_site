25e048a0587b6fdad18aacec837c9fe9ea1c826b
natnael <nsisay49@gmail.com>
2026-03-28 22:50:37 +0300

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Header from './components/Header'
+import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div className='bg-white bg-pattern h-screen'>
-      <Header/>
+    <div className="bg-pattern h-screen">
+      <Header />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,26 @@
-import { Button } from "./ui/button"
+import { Button } from "./ui/button";
 
 const Header = () => {
-    return (
-        <header>
-            <h1>Logo</h1>
-            <nav>
-                <a>Features</a>
-                <a>Solutions</a>
-                <a>Resources</a>
-                <a>Pricing</a>
-            </nav>
-            <div>
-                <Button>Sign in</Button>
-                <Button>Get App</Button>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className="flex items-center justify-between bg-white py-3 px-6 border-b">
+      <h1>Logo</h1>
+      <nav className="flex gap-4">
+        <a>Features</a>
+        <a>Solutions</a>
+        <a>Resources</a>
+        <a>Pricing</a>
+      </nav>
+      <div className="flex gap-4">
+        <Button variant="ghost">Sign in</Button>
+        <Button
+          variant="outline"
+          className="border-primary px-3 rounded-sm py-0.5"
+        >
+          Get App
+        </Button>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
