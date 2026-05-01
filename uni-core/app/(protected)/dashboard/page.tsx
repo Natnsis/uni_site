74525fcdb4bb74748bdee377d336a6f3d2795440
natnael sisay <nsisay49@gmail.com>
2026-05-01 21:12:38 +0300

    beautiful header

--- uni-core/app/(protected)/dashboard/page.tsx
+++ uni-core/app/(protected)/dashboard/page.tsx
@@ -1,6 +1,30 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { BellIcon, GearIcon, UserIcon } from "@phosphor-icons/react"
+
 const Dashboard = () => {
   return (
-    <div>theeh</div>
+    <main>
+      <header className="flex justify-between p-5">
+        <h1 className="text-xl font-bold">UniSite</h1>
+        <div className="flex gap-2 items-center">
+          <div className="flex bg-secondary p-0.5 px-5 border gap-5">
+            <Button className='rounded-full'>Dashboard</Button>
+            <Button variant='ghost'>Students</Button>
+            <Button variant='ghost'>Facaulty</Button>
+            <Button variant='ghost'>Complaits</Button>
+            <Button variant='ghost'>Reports</Button>
+            <Button variant='ghost'>Curriculums</Button>
+            <Button variant='ghost'>Cafeteria</Button>
+            <Button variant='ghost'>Lounges</Button>
+          </div>
+          <Button><GearIcon size={32} /> Setting</Button>
+          <Button><BellIcon size={32} /></Button>
+          <Button><UserIcon size={32} /></Button>
+        </div>
+      </header>
+    </main>
   )
 }
 
