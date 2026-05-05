bbb80a71d74d4eec84554ce723424b2d40dc8cc0
natnael sisay <nsisay49@gmail.com>
2026-05-05 13:41:24 +0300

    complaints side

--- uni-core/app/(protected)/uni-admin/complaints/page.tsx
+++ uni-core/app/(protected)/uni-admin/complaints/page.tsx
@@ -1,6 +1,45 @@
+'use client'
+import ComplaintsTable from "@/components/ComplaintsTable"
+import { CheckIcon, StudentIcon, TimerIcon, UserSwitchIcon } from "@phosphor-icons/react"
+
 const page = () => {
+  const cards = [
+    {
+      icon: StudentIcon,
+      title: 'Student complaints',
+      value: 623
+    },
+    {
+      icon: UserSwitchIcon,
+      title: 'Representative complaints',
+      value: 237
+    },
+    {
+      icon: TimerIcon,
+      title: 'Pending complaints',
+      value: 234
+    },
+    {
+      icon: CheckIcon,
+      title: 'Solved complaints',
+      value: 663
+    },
+  ]
   return (
-    <div>thehe</div>
+    <div className="h-[40%] px-5">
+      <div className="grid grid-cols-4 gap-5">
+        {cards.map((c, index) => (
+          <div className="border p-3 px-5" key={index}>
+            <div className="flex gap-3">
+              <c.icon />
+              <p className="text-sm font-light">{c.title}</p>
+            </div>
+            <h1 className="text-3xl font-bold">{c.value}</h1>
+          </div>
+        ))}
+      </div>
+      <ComplaintsTable />
+    </div>
   )
 }
 
